5aae145ecbbc5a13be600cf2909b72e2973a9beb
eugenesvk <eugenesvk@users.noreply.github.com>
2024-01-08 03:40:16 +0700

    add text command decorator to multiple cursor plugin

--- nv/plugin_multiple_cursors.py
+++ nv/plugin_multiple_cursors.py
@@ -17,7 +17,7 @@
 
 # A port of https://github.com/terryma/vim-multiple-cursors.
 
-from NeoVintageous.nv.plugin import register
+from NeoVintageous.nv.plugin import register, register_text
 from NeoVintageous.nv.vi import seqs
 from NeoVintageous.nv.vi.cmd_base import ViOperatorDef
 from NeoVintageous.nv.vim import ACTION_MODES
@@ -29,6 +29,7 @@ __all__ = []  # type: list
 
 @register(seqs.SEQ['⎈n'], ACTION_MODES)
 @register(seqs.SEQ['gh'], ACTION_MODES)
+@register_text(['MultipleCursorsStart'], ACTION_MODES)
 class MultipleCursorsStart(ViOperatorDef):
     def init(self):
         self.updates_xpos = True
@@ -38,6 +39,7 @@ class MultipleCursorsStart(ViOperatorDef):
 
 @register(seqs.SEQ['⇧j'], (SELECT,))
 @register(seqs.SEQ['⎋'], (SELECT,))
+@register_text(['MultipleCursorsExit'], (SELECT,))
 class MultipleCursorsExit(ViOperatorDef):
     def init(self):
         self.updates_xpos = True
@@ -49,6 +51,7 @@ class MultipleCursorsExit(ViOperatorDef):
 @register(seqs.SEQ['⎈n'], (SELECT,))
 @register(seqs.SEQ['j'], (SELECT,))
 @register(seqs.SEQ['n'], (SELECT,))
+@register_text(['MultipleCursorsAdd'], (SELECT,))
 class MultipleCursorsAdd(ViOperatorDef):
     def init(self):
         self.updates_xpos = True
@@ -58,6 +61,7 @@ class MultipleCursorsAdd(ViOperatorDef):
 
 @register(seqs.ALT_N, (SELECT,))
 @register(seqs.SEQ['⇧a'], (SELECT,))
+@register_text(['MultipleCursorsAddAll'], (SELECT,))
 class MultipleCursorsAddAll(ViOperatorDef):
     def init(self):
         self.scroll_into_view = True
@@ -67,6 +71,7 @@ class MultipleCursorsAddAll(ViOperatorDef):
 @register(seqs.SEQ['⎈p'], (SELECT,))
 @register(seqs.SEQ['k'], (SELECT,))
 @register(seqs.SEQ['⇧q'], (SELECT,))
+@register_text(['MultipleCursorsRemove'], (SELECT,))
 class MultipleCursorsRemove(ViOperatorDef):
     def init(self):
         self.updates_xpos = True
@@ -77,6 +82,7 @@ class MultipleCursorsRemove(ViOperatorDef):
 @register(seqs.SEQ['⎈x'], (SELECT,))
 @register(seqs.SEQ['l'], (SELECT,))
 @register(seqs.SEQ['q'], (SELECT,))
+@register_text(['MultipleCursorsSkip'], (SELECT,))
 class MultipleCursorsSkip(ViOperatorDef):
     def init(self):
         self.scroll_into_view = True
@@ -84,6 +90,7 @@ class MultipleCursorsSkip(ViOperatorDef):
 
 
 @register(seqs.SEQ['g⇧h'], ACTION_MODES)
+@register_text(['MultipleCursorsAll'], ACTION_MODES)
 class MultipleCursorsAll(ViOperatorDef):
     def init(self):
         self.updates_xpos = True

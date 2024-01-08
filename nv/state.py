ae0b9eefbfca9cb4ea54e9a4fe1aa4d157516e4a
eugenesvk <eugenesvk@users.noreply.github.com>
2024-01-08 15:54:52 +0700

    add support for visual default mode
    Menu → Preferences → Settings
    
    ```js
    "vintageous_default_mode": "visual",
    ```
    
    Re https://github.com/NeoVintageous/NeoVintageous/discussions/912
    Re https://github.com/NeoVintageous/NeoVintageous/discussions/959
    Re https://github.com/NeoVintageous/NeoVintageous/discussions/959#discussioncomment-6773509

--- nv/state.py
+++ nv/state.py
@@ -53,6 +53,8 @@ from NeoVintageous.nv.vi.cmd_defs import ViToggleMacroRecorder
 from NeoVintageous.nv.modes import INSERT, INTERNAL_NORMAL, NORMAL, OPERATOR_PENDING, REPLACE, SELECT, UNKNOWN, VISUAL, VISUAL_BLOCK, VISUAL_LINE
 from NeoVintageous.nv.vim import clean_view
 from NeoVintageous.nv.vim import enter_insert_mode
+from NeoVintageous.nv.vim import enter_normal_mode
+from NeoVintageous.nv.vim import enter_visual_mode
 from NeoVintageous.nv.vim import is_visual_mode
 from NeoVintageous.nv.vim import mode_to_name
 from NeoVintageous.nv.vim import reset_status_line
@@ -398,9 +400,16 @@ def init_view(view) -> None:
     if len(view.sel()) == 0:
         view.sel().add(0)
 
-    if get_setting(view, 'default_mode') == 'insert':
-        if mode in (NORMAL, UNKNOWN):
-            enter_insert_mode(view, mode)
+    default_mode = get_setting(view, 'default_mode')
+
+    if default_mode:
+        if default_mode == 'insert':
+            if mode in (NORMAL, UNKNOWN):
+                enter_insert_mode(view, mode)
+        elif default_mode == 'visual':
+            if mode in (NORMAL, UNKNOWN):
+                enter_normal_mode(view, mode)
+                enter_visual_mode(view, mode)
     elif mode in (VISUAL, VISUAL_LINE, VISUAL_BLOCK):
         # Visual modes are not reset (to normal mode), because actions like
         # pressing the super key or opening a command-palette/overlay will cause

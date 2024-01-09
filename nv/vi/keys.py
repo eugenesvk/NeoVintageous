cb85119beedd83e4b5a7459e1a041cd52fd73538
eugenesvk <eugenesvk@users.noreply.github.com>
2024-01-10 03:58:03 +0700

    add mappings_text_reverse dictionary to keys

--- nv/vi/keys.py
+++ nv/vi/keys.py
@@ -51,6 +51,15 @@ mappings_text = {
     VISUAL_BLOCK    	: {},
     VISUAL_LINE     	: {},
 }  # type: dict
+mappings_text_reverse = { # map[internal command Name] = assigned textual command name from ↑ to make matching a textual command to a key easier (also preserves CaSe of entered commands)
+    INSERT          	: {},
+    NORMAL          	: {},
+    OPERATOR_PENDING	: {},
+    SELECT          	: {},
+    VISUAL          	: {},
+    VISUAL_BLOCK    	: {},
+    VISUAL_LINE     	: {},
+}  # type: dict
 
 _NAMED_KEYS_NEST = [
 
@@ -288,6 +297,10 @@ def assign_text(commands:list, modes:tuple, *args, **kwargs):
     def inner(cls):
         for mode in modes:
             for cmd in commands:  # 'EnterInsertMode' → class ViEnterInsertMode(ViOperatorDef)
-                mappings_text[mode][cmd.lower()] = cls(*args, **kwargs)
+                mappings_text        [mode][cmd.lower()                   ] = cls(*args,**kwargs)
+                if (T := type(cls(*args,**kwargs))) in mappings_reverse[mode]:
+                    mappings_text_reverse[mode][T] += [cmd] # add dupe command to a list
+                else:
+                    mappings_text_reverse[mode][T]  = [cmd] # add      command as a list
         return cls
     return inner

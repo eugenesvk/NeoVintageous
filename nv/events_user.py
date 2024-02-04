8ae153568cbea6def084005fc9d4de3dbc440f2f
eugenesvk <eugenesvk@users.noreply.github.com>
2024-02-05 04:04:57 +0700

    fix mode lookup error
    use a dictionary with clean names

--- nv/events_user.py
+++ nv/events_user.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import sublime
 
 from NeoVintageous.nv.modes import INSERT, INTERNAL_NORMAL, NORMAL, OPERATOR_PENDING, REPLACE, SELECT, UNKNOWN, VISUAL, VISUAL_BLOCK, VISUAL_LINE
-from NeoVintageous.nv.modes import M_EVENT, Mode as M, text_to_modes, mode_names_rev
+from NeoVintageous.nv.modes import M_EVENT, Mode as M, text_to_modes, mode_names_rev,mode_clean_names_rev
 from NeoVintageous.nv.rc import cfgU
 from NeoVintageous.nv.cfg_parse import clean_name
 
@@ -50,8 +50,8 @@ def reload_with_user_data_kdl() -> None:
   if hasattr(cfgU,'kdl') and (cfg := cfgU.kdl.get('event',None)): # skip on initial import when Plugin API isn't ready, so no settings are loaded
     global CFG
     for node in cfg.nodes: # (ⓘ)in {(mac)"~/bin" "--var" r#"{"v":1}"#;}
-      mode = mode_names_rev.get(clean_name(node.tag ),None) # ‘Mode.Insert’ for ‘ⓘ’
-      evt  = EVENTrev      .get(clean_name(node.name),None) # ‘enter’       for ‘in’
+      mode = mode_clean_names_rev.get(clean_name(node.tag ),None) # ‘Mode.Insert’ for ‘ⓘ’
+      evt  = EVENTrev            .get(clean_name(node.name),None) # ‘enter’       for ‘in’
       if not mode or\
          not mode in M.Event:
         _log.error("node ‘%s’ has no/unrecognized mode in tag ‘(%s)%s’, skipping"

4b87e35c793120c978d5137da7f1f5f7f43c9a5d
eugenesvk <eugenesvk@users.noreply.github.com>
2024-02-08 02:26:39 +0700

    update log icon

--- nv/log.py
+++ nv/log.py
@@ -3,7 +3,7 @@ import logging
 DEFAULT_LOG_LEVEL = logging.WARNING
 TFMT = '{t.minute:2}:{t.second:2}.{t.microsecond}'
 
-filter_levelname_dict = {'ERROR':'❗','WARNING':'⚠️','INFO':"ⓘ",'KEY':'⌨️','CFG':'Ⓒ'}
+filter_levelname_dict = {'ERROR':'❗','WARNING':'⚠️','INFO':"ⓘ",'KEY':'⌨️','KEYT':'⌨️T','CFG':'Ⓒ'}
 
 def add_module_logger_levels():
   addLoggingLevel('KEYT', DEFAULT_LOG_LEVEL - 20)

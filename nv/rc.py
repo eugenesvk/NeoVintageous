c848d5d8f9bb22387eaf38a8e828dac4fcd9b702
eugenesvk <eugenesvk@users.noreply.github.com>
2024-02-09 14:22:34 +0700

    fix log format error

--- nv/rc.py
+++ nv/rc.py
@@ -362,7 +362,7 @@ def _parse_general_cfg_kdl(general_cfg:kdl.Node,st_pref=None) -> None:
             for arg in node.args:
                 tag = arg.tag   if hasattr(arg,'tag'  ) else ''
                 val = arg.value if hasattr(arg,'value') else arg
-                _log.debug(arg, f"tag={tag}", f"val={val}")
+                _log.debug("%s %s %s", arg, f"tag={tag}", f"val={val}")
                 isSameType = False
                 if     isinstance(    type_def,type):
                     if isinstance(val,type_def):
